fb1ebcb2129ffb0b6d4c53add64d37f8567fff2e
Danielle Dequin <dmdequin@gmail.com>
2023-09-14 16:01:20 +0200

    customized the admin page with tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,51 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    # Add what is listed on admin page
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # Sections displayed based on custom user model
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    # Support for creating users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),  # for custom CSS styling
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+# Assigns the custom UserAdmin class for the model manager
+admin.site.register(models.User, UserAdmin)
