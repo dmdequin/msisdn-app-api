0c540eb84fc859c3af6e72371d966e62109c80b4
Danielle Dequin <dmdequin@gmail.com>
2023-09-29 13:05:29 +0200

    decomposing msisdn_search_view

--- app/msisd/views.py
+++ app/msisd/views.py
@@ -44,30 +44,54 @@ def msisdn_base_view(request):
     return render(request, "base.html", context=context)
 
 
+def in_database(msisdn):
+    """Check if msisd object exists in database."""
+    exists = False
+    if MSISD.objects.filter(msisdn=msisdn).exists():
+        exists = True
+
+    return exists
+
+
+def get_msisd_object(request, msisdn):
+    """Get msisdn object from db and render the page."""
+    msisd_object = MSISD.objects.get(msisdn=msisdn)
+    context = {"object": msisd_object}
+
+    return render(request, "search.html", context)
+
+
+def create_msisd_object(request, **payload):
+    """Create msisd object and render page."""
+    MSISD.objects.create(**payload)
+
+    # Context to pass to html
+    context = {"object": payload,
+               "message": "New number added to database!"}
+
+    return render(request, 'search.html', context)
+
+
 def msisdn_search_view(request, *args, **kwargs):
     """View for searching the MSISD API."""
     query_dict = request.GET  # this is a dictionary
     msisdn = query_dict.get("msisdn")
 
-    msisd_object = None
     if msisdn is not None:
 
-        if MSISD.objects.filter(msisdn=msisdn).exists():
-            # If entry exists in database, get object
-            msisd_object = MSISD.objects.get(msisdn=msisdn)
-            context = {"object": msisd_object}
-
-            return render(request, "search.html", context)
+        if in_database(msisdn):
+            # If entry in database get object and render page
+            return get_msisd_object(request, msisdn)
 
         elif len(msisdn) < 7:
             # If length < minimum valid international number length
-            return render(request,
-                          "base.html",
-                          {"message": "Entry too short, try again."})
+            context = {"message": "Entry too short, try again."}
+            return render(request, "base.html", context)
 
         else:
             # If not in db long enough make format compatible with phonenumbers
             number = "+" + str(msisdn)
+            print(number)
 
             try:
                 # Check if valid phone number
@@ -83,26 +107,18 @@ def msisdn_search_view(request, *args, **kwargs):
                     'subscriber_number': subscriber_number,
                     'country_identifier': country_identifier,
                 }
-                # print(payload)
 
                 if payload['country_identifier'] is None:
                     # If country ID not parsable
-                    return render(request,
-                                  "base.html",
-                                  {"message": "Entry invalid, try again."})
+                    context = {"message": "Entry invalid, try again."}
+                    return render(request, "base.html", context)
 
                 else:
-                    # If number passes create MSISD object
-                    MSISD.objects.create(**payload)
-
-                    # Context to pass to html
-                    context = {"object": payload,
-                               "message": "New number added to database!"}
-
-                    return render(request, 'search.html', context)
+                    # If number passes create MSISD object and render
+                    return create_msisd_object(request, **payload)
 
             except NumberParseException:
                 # If number is not able to be parsed
-                return render(request,
-                              "base.html",
-                              {"message": "Entry invalid, try again."})
+                context = {"message": "Entry invalid, try again."}
+
+                return render(request, "base.html", context)
